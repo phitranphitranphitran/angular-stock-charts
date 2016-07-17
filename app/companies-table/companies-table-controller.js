626d4bf5ad96e806f903372686135abe2933a003
Phi Tran <phitranphitranphitran@gmail.com>
2016-07-17 01:26:55 -0400

    Implement ordering table rows by columns asc/desc

--- app/companies-table/companies-table-controller.js
+++ app/companies-table/companies-table-controller.js
@@ -2,14 +2,20 @@ function CompaniesTableController(stockQuotes) {
 
   stockQuotes.get().then(data => {
     this.companies = data.query.results.quote.map(company => {
-      // simpler property names
+      // simplify property names
       company.symbol = company["Symbol"];
       company.name = company["Name"];
-      company.currentPrice = company["LastTradePriceOnly"];
-      company.marketCap = company["MarketCapitalization"];
+      company.currentPrice = Number(company["LastTradePriceOnly"]).toFixed(2);
+      // market cap is formatted by default in API response
+      company.marketCapFormatted = company["MarketCapitalization"];
+      company.marketCap = marketCapToNum(company["MarketCapitalization"]);
       return company;
     });
   });
+
+  // table rows initially ordered by symbol ascending
+  this.orderByField = "symbol";
+  this.orderReverse = false;
 
   this.setActiveStock = function(index) {
     console.log(this.companies[index]["Symbol"]);
@@ -17,19 +23,40 @@ function CompaniesTableController(stockQuotes) {
 
   // determines if a company row should be shown or hidden in the table
   // according to the user search input
-  this.isNotFiltered = function(index) {
-    if (!this.searchString) {
+  this.isNotFiltered = function(index, searchString) {
+    if (!searchString) {
       return true;
     }
     const company = this.companies[index];
     const name = company.name.toLowerCase();
     const symbol = company.symbol.toLowerCase();
-    const searchString = this.searchString.toLowerCase();
+    searchString = searchString.toLowerCase();
 
     return name.includes(searchString) || symbol.includes(searchString);
   };
+
 }
 
 CompaniesTableController.$inject = ["stockQuotes"];
 
 export default CompaniesTableController;
+
+// converts a formatted market cap string from the API response into a Number
+function marketCapToNum(mcString) {
+  const suffix = mcString.charAt(mcString.length-1);
+  let mcNum;
+  switch(suffix) {
+    // billions
+    case "B":
+      mcNum = Number(mcString.replace("B", "")) * 1000000000;
+      break;
+    // millions
+    case "M":
+      mcNum = Number(mcString.replace("M", "")) * 1000000;
+      break;
+    // < 1 million, no suffix, remove commas
+    default:
+      mcNum = Number(mcString.replace(",", ""));
+  }
+  return mcNum;
+}

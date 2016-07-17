1edc4f5381561925fdf1ab511476a027bdcba09a
Phi Tran <phitranphitranphitran@gmail.com>
2016-07-17 04:49:52 -0400

    Transform API data points into highcharts series

--- app/price-history-chart/price-history-chart-controller.js
+++ app/price-history-chart/price-history-chart-controller.js
@@ -1,18 +1,51 @@
-function PriceHistoryChartController() {
+function PriceHistoryChartController(stockHistories) {
+  stockHistories.get().then(data => {
+    this.histories = {};
+    // transform raw API data points to a hash of data series for each company
+    data.query.results.quote.forEach(dataPoint => {
+      const symbol = dataPoint["Symbol"];
+      if (!this.histories.hasOwnProperty(symbol)) {
+        this.histories[symbol] = [];
+      }
+      // convert date string from API to unix time
+      const date = dataPoint["Date"].split("-").map(s => Number(s));
+      const year = date[0];
+      const month = date[1] - 1; // JS Date constructor expects 0-11 for month
+      const day = date[2];
+      const time = (new Date(year, month, day)).getTime();
+
+      const price = +Number(dataPoint["Close"]).toFixed(2);
+
+      this.histories[symbol].push([time, price]);
+    });
+    console.log(this.histories);
+    this.chartConfig.series[0].data = this.histories["XOM"];
+  });
+
+
+
   this.chartConfig = {
     options: {
       chart: {
-        type: "bar"
+        type: "line"
       }
     },
-    series: [{
-      data: [10, 15, 12, 8, 7]
-    }],
     title: {
       text: "Hello"
     },
-    loading: false
+    xAxis: {
+      title: { text: "Date" },
+      type: "datetime"
+    },
+    yAxis: {
+      title: { text: "Stock Price (USD)" }
+    },
+    series: [{
+      // name
+    }]
   };
 }
+
+PriceHistoryChartController.$inject = ["stockHistories"];
 
 export default PriceHistoryChartController;
